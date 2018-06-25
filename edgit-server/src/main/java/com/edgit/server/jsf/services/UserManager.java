cad48010e5951e2e8866c7c27c28afaa180505f5
Eduardo <Eduardo@DESKTOP-IJFAGD8.home>
2018-06-25 22:19:35 +0100

    Nova funcionalidade: sign out do utilizador. Template do cabeçalho alterado devido a esta nova funcionalidade.

--- edgit-server/src/main/java/com/edgit/server/jsf/services/UserManager.java
+++ edgit-server/src/main/java/com/edgit/server/jsf/services/UserManager.java
@@ -3,8 +3,11 @@ package com.edgit.server.jsf.services;
 import java.io.Serializable;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+@Named
 @SessionScoped
 public class UserManager implements Serializable {
 
@@ -23,6 +26,16 @@ public class UserManager implements Serializable {
 		currentUser = user;
 		return "homepage"; // go to home page
 	}
+	
+	public boolean isSignedIn() {
+		return currentUser != null;
+	}
+	
+
+	public String signOut() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		return "welcome?faces-redirect=true";
+	}
 
 	public User getCurrentUser() {
 		return currentUser;
@@ -33,7 +46,7 @@ public class UserManager implements Serializable {
 	}
 
 	public String saveUser(User user) {
-		userService.saveUser(user); 
+		userService.saveUser(user);
 		currentUser = user;
 		return "homepage"; // Home Page
 	}
