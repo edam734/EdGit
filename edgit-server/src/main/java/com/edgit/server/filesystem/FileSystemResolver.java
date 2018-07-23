f40e25a82a6f3b081b81dec97deca1737d1ada8f
Eduardo <Eduardo@DESKTOP-IJFAGD8.home>
2018-07-23 18:51:28 +0100

    Classes utilitárias para funcionalidades relacionadas com o sistema de ficheiros.

---
+++ edgit-server/src/main/java/com/edgit/server/filesystem/FileSystemResolver.java
@@ -0,0 +1,10 @@
+package com.edgit.server.filesystem;
+
+import java.io.File;
+
+public class FileSystemResolver {
+
+	public static void makeDirectory(File file) {
+		Utils.makeDirectory(file);
+	}
+}
